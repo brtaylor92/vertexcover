0b78df047fead8cf4c52249e7e5c856f38d3a7f9
Benjamin Taylor <taylo886@msu.edu>
2015-03-14 17:59:16 -0500

    Passes test cases 0 - 3 (20 vertices)

--- proj.cc
+++ proj.cc
@@ -24,25 +24,43 @@ class MinCover {
 public:
   MinCover() = delete;
   MinCover(istream &is) : in(is), N(*(in++)), M(*(in++)), G(N * N, false) {
-    for (int i = 0; i < N; i++) {
+    /*for (int i = 0; i < N; i++) {
       G.at(i * N + i) = true;
-    }
+    }*/
     for (int i = 0; i < M; i++) {
       int v1 = *(in++), v2 = *(in++);
       G.at(v2 + v1 * N) = true;
       G.at(v1 + v2 * N) = true;
     }
-
   }
   MinCover(const MinCover &other) = delete;
   MinCover(MinCover &&other) = default;
   virtual ~MinCover() = default;
   int findMin() {
-    vector<bool> tree(N, 0);
-    return min(examineVertices(0, tree), examineVertices(0, tree, false));
+    vector<bool> tree(N, false);
+    return min(examineVertices(0, tree, true), examineVertices(0, tree, false));
   }
-  int examineVertices(int vertex, vector<bool> tree, bool use = true);
-  friend ostream& operator<<(ostream &out, const MinCover &mc) {
+  int examineVertices(int vertex, vector<bool> tree, bool use) {
+    tree.at(vertex) = use;
+    ++vertex;
+    vector<bool> test(G);
+    for (int i = 0; i < vertex; i++) {
+      if (tree.at(i)) {
+        for (int j = 0; j < N; j++) {
+          test.at(j + i * N) = false;
+          test.at(i + j * N) = false;
+        }
+      }
+    }
+    if (!accumulate(begin(test), end(test), 0)) {
+      return accumulate(begin(tree), end(tree), 0);
+    } else if (vertex == N) {
+      return N;
+    }
+    return min(examineVertices(vertex, tree, true),
+               examineVertices(vertex, tree, false));
+  }
+  friend ostream &operator<<(ostream &out, const MinCover &mc) {
     for (int i = 0; i < mc.N; i++) {
       for (int j = 0; j < mc.N; j++) {
         out << mc.G.at(j + i * mc.N) << " ";
@@ -51,6 +69,7 @@ public:
     }
     return out;
   }
+
 private:
   istream_iterator<int> in;
   int N;
@@ -58,20 +77,10 @@ private:
   vector<bool> G;
 };
 
-int MinCover::examineVertices(int vertex, vector<bool> tree, bool use) {
-  tree.at(vertex) = use;
-  ++vertex;
-  if (/* covered */ false || vertex == N) {
-    return vertex;
-  }
-  return min(examineVertices(vertex, tree),
-             examineVertices(vertex, tree, false));
-}
-
 int main() {
   // Read input
-  //istream_iterator<int> in(cin);
-  //int N = *(in++), M = *(in++);
+  // istream_iterator<int> in(cin);
+  // int N = *(in++), M = *(in++);
 
   // Pick edges, construct set of at most 2x vertices
   // Use this to bound size of solution subsets examined
@@ -96,14 +105,13 @@ int main() {
   // cout << "Min soln: " << S.size()/2 << endl;
 
   // Print adjacency matrix
-  
 
   // Woo, I don't know how to look at all possible subsets
   // Seriously, who thought letting me write this was a good idea?
-  //vector<vector<bool>> possible(exp2(N), vector<bool>(N, false));
-  //vector<bool> tree(N, false);
+  // vector<vector<bool>> possible(exp2(N), vector<bool>(N, false));
+  // vector<bool> tree(N, false);
   MinCover mc(cin);
-  // cout << mc << endl;
+  //cout << mc << endl;
   cout << mc.findMin() << endl;
   return 0;
 }
