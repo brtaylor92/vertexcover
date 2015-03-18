cad23022b54e972aba780863e86a907f05b1cbe3
Benjamin Taylor <taylo886@msu.edu>
2015-03-18 04:50:19 -0400

    Add some helper functions and further reduce line count

--- proj.cc
+++ proj.cc
@@ -17,15 +17,13 @@ using std::vector;
 
 class MinCover {
 public:
-  MinCover() = delete;
   MinCover(istream &is)
-      : in(is), N(*in), M(*++in), G(N * N, false), backups(N) {
+      : in(is), N(*in), M(*++in), min_soln(N), G(N * N, false), backups(N) {
     for (int32_t i = 0; i < M; ++i) {
       int32_t v1 = *++in, v2 = *++in;
       G.at(v1 + v2 * N) = true;
       G.at(v2 + v1 * N) = true;
     }
-    min_soln = min(M, N);
   }
   MinCover(const MinCover &other) = delete;
   MinCover(MinCover &&other) = delete;
@@ -35,9 +33,7 @@ public:
     for (int32_t i = 0; i < N * N; ++i) {
       if (G.at(i)) {
         int32_t new_v1 = i / N, new_v2 = i % N;
-        int32_t new_deg =
-            count(begin(G) + new_v1 * N, begin(G) + (new_v1 + 1) * N, true) +
-            count(begin(G) + new_v2 * N, begin(G) + (new_v2 + 1) * N, true);
+        int32_t new_deg = getDegree(new_v1) + getDegree(new_v2);
         if (new_deg > deg) {
           v1 = new_v1;
           v2 = new_v2;
@@ -50,50 +46,33 @@ public:
     return min(lb, rb);
   }
   int32_t examineVertex(int32_t v, int32_t d, int32_t sz) {
-    backups.at(d) = G;
+    backups.at(d++) = G;
     int32_t oldM = M;
     ++sz;
-    ++d;
-    for (int32_t i = 0; i < N; ++i) {
-      if (G.at(i + v * N)) {
-        G.at(i + v * N) = false;
-        G.at(v + i * N) = false;
-        --M;
-      }
-    }
-    bool foundone = false;
-    do {
+    removeVertex(v);
+    bool foundone = true;
+    while (foundone) {
       foundone = false;
       for (int32_t i = 0; i < N; ++i) {
-        int32_t deg = count(begin(G) + i * N, begin(G) + (i + 1) * N, true);
+        int32_t deg = getDegree(i);
         if (deg == 1) {
-          auto it = find(begin(G) + i * N, begin(G) + (i + 1) * N, true);
-          int32_t neighbor = distance(begin(G) + i * N, it);
-          for (int32_t j = 0; j < N; ++j) {
-            if (G.at(j + neighbor * N)) {
-              G.at(j + neighbor * N) = false;
-              G.at(neighbor + j * N) = false;
-              --M;
-            }
-          }
           ++sz;
           foundone = true;
+          auto it = find(begin(G) + i * N, begin(G) + (i + 1) * N, true);
+          removeVertex(distance(begin(G) + i * N, it));
         }
       }
-    } while (foundone);
+    }
     if (M) {
       if (sz + 1 >= min_soln) {
-        G.swap(backups.at(d - 1));
-        M = oldM;
+
         return N;
       }
       int32_t deg = -1, v1 = -1, v2 = -1;
       for (int32_t i = 0; i < N * N; ++i) {
         if (G.at(i)) {
           int32_t new_v1 = i / N, new_v2 = i % N;
-          int32_t new_deg =
-              count(begin(G) + new_v1 * N, begin(G) + (new_v1 + 1) * N, true) +
-              count(begin(G) + new_v2 * N, begin(G) + (new_v2 + 1) * N, true);
+          int32_t new_deg = getDegree(new_v1) + getDegree(new_v2);
           if (new_deg > deg) {
             v1 = new_v1;
             v2 = new_v2;
@@ -113,6 +92,18 @@ public:
     G.swap(backups.at(d - 1));
     M = oldM;
     return sz;
+  }
+  inline void removeVertex(int32_t v) {
+    for (int i = 0; i < N; i++) {
+      if (G.at(i + v * N)) {
+        G.at(i + v * N) = false;
+        G.at(v + i * N) = false;
+        --M;
+      }
+    }
+  }
+  inline int32_t getDegree(int32_t v) {
+    return count(begin(G) + v * N, begin(G) + (v + 1) * N, true);
   }
 
 private:
