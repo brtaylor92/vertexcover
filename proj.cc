2f9d1c033e651ba43cbcea399bc8191fbf8aaa96
Benjamin Taylor <taylo886@msu.edu>
2015-04-06 09:15:50 -0400

    Refactoring

--- proj.cc
+++ proj.cc
@@ -17,18 +17,18 @@ using std::vector;
 class MinCover {
 public:
   MinCover(istream &is)
-      : in(is), N(*in), M(*++in), minSoln(N), G(N * N, false), degrees(N),
-        backupGs(N), backupDegrees(N) {
+      : in(is), N(*in), M(*++in), minSoln(N), G(N * N, false), deg(N),
+        backupGs(N), backupDegs(N) {
     for (int32_t i = 0; i < M; ++i) {
       int32_t v1 = *++in, v2 = *++in;
       G.at(v1 + v2 * N) = G.at(v2 + v1 * N) = true;
-      ++degrees.at(v1);
-      ++degrees.at(v2);
+      ++deg.at(v1);
+      ++deg.at(v2);
     }
   }
   ~MinCover() = default;
-  int32_t addToCover(int32_t d, int32_t sz) {
-    // Remove cliques/degree one vertices; this is always useful
+  int32_t addToCover(int32_t d = 0, int32_t sz = 0) {
+    // Remove cliques/degree one vertices; these must always be in the cover
     while (int32_t numRemoved = removeClique()) {
       sz += numRemoved;
     }
@@ -38,29 +38,28 @@ public:
       return sz;
     }
     // Find the highest degree vertex
-    int32_t v =
-        distance(begin(degrees), max_element(begin(degrees), end(degrees)));
+    int32_t v = distance(begin(deg), max_element(begin(deg), end(deg)));
     // If M/d(v) is larger than the best cover, bound
-    if (M / degrees.at(v) + sz >= minSoln) {
+    if (M / deg.at(v) + sz >= minSoln) {
       return N;
     }
-    // Save the graph and related information before removing other vertices
+    // Save the graph and related information before removing optional vertices
     int32_t oldM = M;
-    backupDegrees.at(d) = degrees;
+    backupDegs.at(d) = deg;
     backupGs.at(d++) = G;
-    // Consider the highest degree vertex as the best next choice for the cover
+    // Consider the highest degree vertex as the best next choice of cover
     vector<int32_t> best(1, v);
-    int32_t bestDeg = degrees.at(v);
-    // Check if there is better path by taking the neighbors of deg 2 vertices
+    int32_t bestDeg = deg.at(v);
+    // Check if there is better branch taking the neighbors of deg 2 vertices
     for (int i = 0; i < N; ++i) {
-      if (degrees.at(i) == 2) {
+      if (deg.at(i) == 2) {
         vector<int32_t> current;
         current.reserve(2);
         int32_t currentDeg = 0;
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N; ++j) {
           if (G.at(j + i * N)) {
             current.push_back(j);
-            currentDeg += degrees.at(j);
+            currentDeg += deg.at(j);
           }
         }
         if (currentDeg > bestDeg) {
@@ -75,7 +74,7 @@ public:
     int32_t likely = addToCover(d, sz + best.size());
     // Restore those vertices before taking the other branch
     M = oldM;
-    degrees.swap(backupDegrees.at(d - 1));
+    deg.swap(backupDegs.at(d - 1));
     G.swap(backupGs.at(d - 1));
     // Look at the neighbors of the "best" cover additions for the other branch
     // If the "best" choices are not part of the min cover, their neighbors are
@@ -91,14 +90,14 @@ public:
   }
   bool formsClique(int32_t v) {
     vector<int32_t> n;
-    n.reserve(degrees.at(v));
+    n.reserve(deg.at(v));
     for (int32_t i = 0; i < N; ++i) {
       if (G.at(i + v * N)) {
         n.push_back(i);
       }
     }
     for (auto i : n) {
-      if (degrees.at(i) < degrees.at(v)) {
+      if (deg.at(i) < deg.at(v)) {
         return false;
       }
       for (auto j : n) {
@@ -112,7 +111,7 @@ public:
   int32_t removeClique() {
     int32_t removed = 0;
     for (int32_t i = 0; i < N; ++i) {
-      if (degrees.at(i) && formsClique(i)) {
+      if (deg.at(i) && formsClique(i)) {
         for (int32_t j = 0; j < N; ++j) {
           if (G.at(j + i * N)) {
             removed += removeVertex(j);
@@ -123,11 +122,11 @@ public:
     return removed;
   }
   int32_t removeVertex(int32_t v) {
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; ++i) {
       if (G.at(i + v * N)) {
         G.at(i + v * N) = G.at(v + i * N) = false;
-        --degrees.at(v);
-        --degrees.at(i);
+        --deg.at(v);
+        --deg.at(i);
         --M;
       }
     }
@@ -138,9 +137,9 @@ private:
   istream_iterator<int32_t> in;
   int32_t N, M, minSoln;
   vector<bool> G;
-  vector<int32_t> degrees;
+  vector<int32_t> deg;
   vector<vector<bool>> backupGs;
-  vector<vector<int32_t>> backupDegrees;
+  vector<vector<int32_t>> backupDegs;
 };
 
-int main() { cout << MinCover(cin).addToCover(0, 0) << "\n"; }
+int main() { cout << MinCover(cin).addToCover() << "\n"; }
