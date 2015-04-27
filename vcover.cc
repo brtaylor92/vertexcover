28b9043cd249fc6d001bb18ee39590700b831f32
Benjamin Taylor <taylo886@msu.edu>
2015-04-26 21:04:41 -0400

    Do bounding within polynomial ops. Because why not?

--- vcover.cc
+++ vcover.cc
@@ -43,53 +43,81 @@ public:
     return minSoln;
   }
   void findMinCover() {
-    removeCliques();
-    const int32_t in = soln.CountIn();
-    if (soln.IsFinal()) {
-      minSoln = min(minSoln, in);
-      return;
-    } else if (in + 1 > minSoln) {
-      return;
-    }
-    // Find the highest degree vertex
-    size_t allowed = minSoln - in - 1;
-    vector<int32_t> heap;
-    heap.reserve(minSoln - in - 1);
-    int32_t v = -1, bestDeg = 0, totalDeg = 0;
-    for (int32_t i = soln.GetNextUnk(-1); i != -1; i = soln.GetNextUnk(i)) {
-      const int32_t d = G.GetMaskedDegree(i, soln.GetUnkVector());
-      totalDeg += d;
-      if (d > bestDeg) {
-        v = i;
-        bestDeg = d;
-      }
-      if (heap.size() < allowed || (heap.size() && d > heap.at(0))) {
-        if (heap.size() < allowed) {
-          heap.push_back(d);
-        } else {
-          pop_heap(begin(heap), end(heap), greater<int32_t>());
-          heap.back() = d;
+    bool polyOps = true;
+    int32_t v = -1, bestDeg = 0;
+    while (polyOps) {
+      polyOps = false;
+
+      for (int32_t i = soln.GetNextUnk(-1); i != -1; i = soln.GetNextUnk(i)) {
+        // If degree 0, exclude
+        if (!G.GetMaskedDegree(i, soln.GetUnkVector())) {
+          soln.Exclude(i);
+        } else if (formsClique(i)) {
+          // If we have a clique, include the neighbors and exclude this node
+          soln.IncludeSet(G.GetEdgeSet(i));
+          soln.Exclude(i);
+          polyOps = true;
         }
-        push_heap(begin(heap), end(heap), greater<int32_t>());
       }
-    }
-    int32_t max = accumulate(begin(heap), end(heap), 0);
-    // If best remaining vertices are larger than the best cover, bound
-    if (max < totalDeg / 2) {
-      return;
+      const int32_t in = soln.CountIn();
+      if (soln.IsFinal()) {
+        minSoln = min(minSoln, in);
+        return;
+      } else if (in + 1 > minSoln) {
+        return;
+      }
+      // Find the highest degree vertex
+      size_t allowed = minSoln - in - 1;
+      vector<int32_t> heap;
+      heap.reserve(minSoln - in - 1);
+      int32_t totalDeg = 0;
+      for (int32_t i = soln.GetNextUnk(-1); i != -1; i = soln.GetNextUnk(i)) {
+        const int32_t d = G.GetMaskedDegree(i, soln.GetUnkVector());
+        totalDeg += d;
+        if (d > bestDeg) {
+          v = i;
+          bestDeg = d;
+        }
+        if (heap.size() < allowed || (heap.size() && d > heap.at(0))) {
+          if (heap.size() < allowed) {
+            heap.push_back(d);
+          } else {
+            pop_heap(begin(heap), end(heap), greater<int32_t>());
+            heap.back() = d;
+          }
+          push_heap(begin(heap), end(heap), greater<int32_t>());
+        }
+      }
+      int32_t max = accumulate(begin(heap), end(heap), 0);
+      // If best remaining vertices are larger than the best cover, bound
+      if (max < totalDeg / 2) {
+        return;
+      }
     }
     // Try to beat the highest degree with a pair of neighbors of deg-2 vertex
     vector<int32_t> best(1, v), current;
-    int32_t currentDeg = 0;
     for (int32_t i = soln.GetNextUnk(-1); i != -1; i = soln.GetNextUnk(i)) {
       const int32_t d = G.GetMaskedDegree(i, soln.GetUnkVector());
       if (d == 2) {
-        current.clear();
-        currentDeg = 0;
-        const BitVector &e = G.GetEdgeSet(i) & soln.GetUnkVector();
-        for (int32_t j = e.FindBit(); j != -1; j = e.FindBit(++j)) {
-          current.push_back(j);
-          currentDeg += G.GetMaskedDegree(j, soln.GetUnkVector());
+        const BitVector &n = G.GetEdgeSet(i) & soln.GetUnkVector();
+        int32_t v1 = n.FindBit();
+        int32_t v2 = n.FindBit(v1 + 1);
+        current = {v1, v2};
+        int32_t currentDeg = G.GetMaskedDegree(v1, soln.GetUnkVector()) +
+                             G.GetMaskedDegree(v2, soln.GetUnkVector());
+        for (size_t j = 0; j < current.size(); ++j) {
+          const BitVector &e =
+              G.GetEdgeSet(current.at(j)) & soln.GetUnkVector();
+          for (int32_t k = e.FindBit(); k != -1; k = e.FindBit(++k)) {
+            if (G.GetMaskedDegree(k, soln.GetUnkVector()) == 2 &&
+                find(begin(current), end(current), k) == end(current)) {
+              int32_t far = (G.GetEdgeSet(k) & soln.GetUnkVector()).FindBit();
+              if (find(begin(current), end(current), far) == end(current)) {
+                current.push_back(far);
+                currentDeg += G.GetMaskedDegree(far, soln.GetUnkVector());
+              }
+            }
+          }
         }
         if (currentDeg > bestDeg) {
           best = current;
@@ -125,19 +153,6 @@ public:
     }
     // V and its neighbors form a clique
     return true;
-  }
-  void removeCliques() {
-    for (int32_t i = soln.GetNextUnk(-1); i != -1; i = soln.GetNextUnk(i)) {
-      // If degree 0, exclude
-      if (!G.GetMaskedDegree(i, soln.GetUnkVector())) {
-        soln.Exclude(i);
-      } else if (formsClique(i)) {
-        // If we have a clique, include the neighbors and exclude this node
-        soln.IncludeSet(G.GetEdgeSet(i));
-        soln.Exclude(i);
-        i = -1;
-      }
-    }
   }
 
 private:
